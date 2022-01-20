15084f6371ecfcde2dff0f284c04ca828348fd58
M4YX0R <trincitasdad@gmail.com>
2022-01-20 15:47:18 +0300

    even better formula, nerf

--- src/Organism/Cell/BodyCells/FatCell.js
+++ src/Organism/Cell/BodyCells/FatCell.js
@@ -22,7 +22,8 @@ class FatCell extends BodyCell{
             return;
         if (n_cell.state == CellStates.food){
             env.changeCell(n_cell.col, n_cell.row, CellStates.empty, null);
-            this.org.lifetime=this.org.lifetime - this.org.lifetime/(this.org.foodNeeded()*this.org.foodNeeded());
+            
+            this.org.lifetime -= Math.random()*this.org.lifetime/(this.org.foodNeeded()*this.org.foodNeeded());
         }
     }
 }
